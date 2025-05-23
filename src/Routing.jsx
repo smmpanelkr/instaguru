b2ed5d13722d269f045049222662a250892bf34f
mrsonukr <MSSONUKR@GMAIL.COM>
2025-05-23 16:22:34 +0530

    2nd

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -3,17 +3,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Purchase from "./pages/Purchase";
 import Contact from "./pages/Contact";
-import Products from "./pages/Products"; // Import the Products page
+import Products from "./pages/Products";
+import ScrollToTop from "./components/ScrollToTop"; // make sure the path is correct
 
 const Routing = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/purchase" element={<Purchase />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/:slug" element={<Products />} />{" "}
+        <Route path="/:slug" element={<Products />} />
       </Routes>
     </Router>
   );
